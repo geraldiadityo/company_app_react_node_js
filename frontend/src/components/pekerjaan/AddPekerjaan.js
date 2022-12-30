ba8267fff73be0e295394e3e032ee5f1fa6ae39d
geraldi <geraldi19adityo@gmail.com>
2022-12-30 20:29:30 +0700

    fixed bug cannot upload file in upload pekerjaans

--- frontend/src/components/pekerjaan/AddPekerjaan.js
+++ frontend/src/components/pekerjaan/AddPekerjaan.js
@@ -1,14 +1,22 @@
-import React, {useState} from "react";
-import UploadService from "./service/FileUploadService";
+import React, {useState, useEffect} from "react";
+import authHeader from "../../service/auth-header";
+import axios from "axios";
+import Select from "react-select";
+import UploadFile from "./service/FileUploadService";
 
 
 const PekerjaanAdd = (props) => {
     const [nama, setNama] = useState("");
     const [currentFile, setCurrentFile] = useState(undefined);
     const [progress, setProgress] = useState(0);
+    const [employee, setEmployee] = useState("");
+    const [employess, setEmployees] = useState([]);
+
+    const API_URL_EMPLOYEE = "http://localhost:8080/api/employee";
 
     const selectFile = (event) => {
         setCurrentFile(event.target.files[0]);
+        setProgress(Math.round((100*event.loaded)/ event.total));
         setProgress(0);
     };
 
@@ -16,24 +24,40 @@ const PekerjaanAdd = (props) => {
         setNama(value);
     }
 
-    const addPekerjaan = async() => {
+    const onChangeEmployee = (value) => {
+        setEmployee(value);
+    }
+
+    const getDataEmployee = async() => {
+        await axios.get(API_URL_EMPLOYEE,{
+            headers:authHeader()
+        }).then((res) => {
+            const result = res.data.data.map(data => {
+                return {
+                    label:data.ename,
+                    value:data.ename,
+                }
+            })
+            setEmployees(result);
+        });
+    };
+
+
+    useEffect(() => {
+        getDataEmployee();
+    },[]);
+
+    const addPekerjaan = async(e) => {
+        e.preventDefault();
         setProgress(0);
-        UploadService.upload(nama,currentFile,(event) => {
-            setProgress(Math.round((100*event.loaded)/ event.total));
+        await UploadFile(nama,currentFile,employee,(e) => {
+            setProgress(Math.round((100*e.loaded)/ e.total));
         }).then((res) => {
             props.resetState();
             props.toggle();
-            props.setMessageNotif(res.data.message,"success");
-            props.toggleNotif();
-        }).catch((err) => {
-            setProgress(0);
-            setCurrentFile(undefined);
-            setNama("");
-            props.toggle();
-            props.setMessageNotif(err.message,"danger");
+            props.setMessageNotif(res.data.message, "success");
             props.toggleNotif();
         });
-
     };
 
     const defaultEmpty = (value) => {
@@ -44,11 +68,11 @@ const PekerjaanAdd = (props) => {
         <form onSubmit={addPekerjaan}>
             <div className="form-group">
                 <label htmlFor="nama">Nama Pekerjaan</label>
-                <input type="text" name="nama" className="form-control" value={defaultEmpty(nama)} onChange={(e) => onChangeNama(e.target.value)}/>
+                <input type="text" className="form-control" value={defaultEmpty(nama)} onChange={(e) => onChangeNama(e.target.value)}/>
             </div>
             <div className="form-group">
                 <label htmlFor="filekerja">File Kerja</label>
-                <input type="file" className="form-control" onChange={selectFile}/>
+                <input type="file" accept="*" className="form-control" onChange={selectFile}/>
                 {currentFile && (
                     <div className="progress">
                         <div className="progress-bar" role="progressbar"
@@ -60,6 +84,13 @@ const PekerjaanAdd = (props) => {
                         </div>
                     </div>
                 )}
+            </div>
+            <div className="form-group">
+                <Select
+                className="form-control"
+                options={employess}
+                onChange={(e) => onChangeEmployee(e.value)}
+                />
             </div>
             <div className="form-group">
                 <div></div>
--- frontend/src/components/pekerjaan/service/FileUploadService.js
+++ frontend/src/components/pekerjaan/service/FileUploadService.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 const user = JSON.parse(localStorage.getItem("user"));
-const API_URL = "http://localhost:8080/api/pekerjaan/"
+const API_URL = "http://localhost:8080/api/pekerjaan/";
 
-const upload = (nama,file, onUploadProgress) => {
+const UploadFile = (nama,file,employee, onUploadProgress) => {
     let formData = new FormData();
 
     formData.append("nama",nama);
     formData.append("file",file);
+    formData.append("employee",employee);
 
     return axios.post(API_URL,formData,{
         headers:{
@@ -18,4 +19,4 @@ const upload = (nama,file, onUploadProgress) => {
     });
 };
 
-export default upload;
+export default UploadFile;
